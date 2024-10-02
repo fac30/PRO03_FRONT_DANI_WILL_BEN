1d94ceede375913f5b941013b249230aab7c8647
Danielle <daniellelee73@gmail.com>
2024-10-02 15:45:54 +0100

    Co-authored-by: william-man <william-man@users.noreply.github.com>

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+//Home page
+//Stretch goal, fix the colour line at the right of the image. 
+// Fix the logo at the end of the image. 
+
+import Button from '../components/Button'
+
+const Home = () => {
+    const buttonContainerStyle: string = "absolute bottom-[15%] left-1/2 transform -translate-x-1/2";
+    const buttonStyle: string = "bg-button-colour text-black py-2 px-4 rounded-full w-32";
+    
+    return (
+    <>
+      <div className="min-h-screen bg-contain bg-center bg-no-repeat bg-home-background bg-[url('/src/assets/gamelogo.jpg')]">
+        <div className={buttonContainerStyle}>
+            <Button label={"Start"} onClick={()=>{}} classes={buttonStyle} />
+        </div>
+      </div>
+     
+      </>
+    );
+  };
+
+export default Home;
+  
